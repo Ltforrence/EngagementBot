751badd5df17e32c9818f508aba6f988277ff18b
Ltforrence <32022170+Ltforrence@users.noreply.github.com>
2021-01-12 18:51:51 -0500

    added some stuff. Will probably not push this

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ logger = logging.getLogger()
 
 
 def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
 
 
 
